74c742ab094ac8b295c4d4ea154e8de74eff15ad
DanilKarabinovskyi <dankrbn@gmail.com>
2022-04-16 22:07:16 +0300

    Add movies,favorite, detail item

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -3,41 +3,46 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
+import com.example.movieapp.core.presentation.Navigation
+import com.example.movieapp.presentation.feed.MoviesScreen
+import com.example.movieapp.presentation.feed.MoviesScreenViewModel
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cardsViewModel by viewModels<MoviesScreenViewModel>()
+
+    @OptIn(ExperimentalCoilApi::class, androidx.compose.ui.ExperimentalComposeUiApi::class,
+        androidx.compose.material.ExperimentalMaterialApi::class
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val scaffoldState = rememberScaffoldState()
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    Navigation(
+                        navController,
+                        cardsViewModel = cardsViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieapp/ui/theme/Theme.kt
@@ -7,24 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = purple200,
+    primaryVariant = purple700,
+    secondary = teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = purple500,
+    primaryVariant = purple700,
+    secondary = teal200
 )
 
 @Composable
